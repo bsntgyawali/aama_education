ab122015297ad0ab9315cbb3e27291b72a9604fd
TMSuraj <er.surajofficial92@gmail.com>
2025-12-06 10:45:39 +0545

    Update Navbar.jsx
    updated navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,28 @@
 import logo from "../assets/logo.png";
+import "./navbar.css";
 
-const Navbar = () =>{
+const Navbar = ()=>{
     return(
-        <header className="header">
-            <img className="header__logo" src={logo} alt="logo of institute" />
-            <nav className="header__navlinks">
-                <ul className="header__navlinks__links">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+       <header className="header">
+        <img className="header__logo" src={logo} alt="company logo" />
+        <nav className="header__navigation">
+            <div className="navigation__barmenu">
+                <dvi className="bar"></dvi>
+                <dvi className="bar"></dvi>
+                <dvi className="bar"></dvi>
+            </div>
+             <ul className="navigation__links">
+                    <li><a>Home</a></li>
+                    <li><a>About Us</a></li>
+                    <li><a>Services</a></li>
+                    <li><a>Events</a></li>
+            </ul>
+            <button className="navigation__btn"> contact us</button>
+
+        </nav>
+       </header>
     );
+
 }
 
 export default Navbar;
