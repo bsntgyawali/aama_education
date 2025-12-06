e550b6bd5a643960a35e40b9ade4a35e5a2d1cc1
TMSuraj <er.surajofficial92@gmail.com>
2025-12-06 10:53:02 +0545

    Update Footer.jsx
    added new components in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,65 @@
-const Footer = () =>{
-    return(
-        <footer className="footer">
-            <div className="footer__navlinks">
-                <div className="section__location">
-                    <h3>Location</h3>
-                    <ul>
-                        <li>Butwal-8, Milanchowk</li>
-                        <li>9860559643, 9762899976</li>
-                        <li>info@aamaeducation.edu.np</li>
-                    </ul>
-                </div>
-                <div className="section__quick-links">
-                    <h3>Navigate</h3>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Services</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-                <div className="section__social-media">
-                    <h3>Follow Us</h3>
-                    <ul>
-                        <li>LinkedIn</li>
-                        <li>Facebook</li>
-                        <li>Instagram</li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <p>Copyright &copy; 2025 Aama Education Services</p>
-                <div>
-                    <li>Privacy Policy</li>
-                    <li>Cookies</li>
-                </div>
+import logo from "../assets/logo.jpg";
+import location from "../assets/location.svg";
+import phone from "../assets/phone.svg";
+import mail from "../assets/mail.svg";
+import facebook from "../assets/facebook.svg";
+import insta from "../assets/instagram.svg";
+import linkedIn from "../assets/linkedIn.svg";
+import "./footer.css";
 
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                <div>
+                    <h4>Subscribe</h4>
+                    <p>Join our community to receive updates</p>
+                    <form className="subscribe__form">
+                        <input type="email" placeholder="Enter your email..." />
+                        <button> Subscribe</button>
+                    </form>
+                    <p>By subscribing, you agree to our Privacy Policy.</p>
+                </div>
+                <div>
+                    <h4>Contact Info</h4>
+                    <ul>
+                        <li><img className="contact-icon" src={location} alt="pin icon" />Butwal-8, Milanchowk</li>
+                        <li><img className="contact-icon" src={phone} alt="phone icon" />9860559643</li>
+                        <li><img className="contact-icon" src={mail} alt="mail icon" />info@aamaeducation.edu.np</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>About Us</a></li>
+                        <li><a>Services</a></li>
+                        <li><a>Events</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <img className="footer__logo" src={logo} alt="company logo" />
+                    <p>
+                        The institution that has been working rigourously<br/>
+                        to help students achieve thier international dreaam.
+                    </p>
+                    <ul>
+                        <li><a><img className="media-icon" src={facebook} alt="facebook logo" /></a></li>
+                        <li><a><img className="media-icon" src={insta} alt="insta logo" /></a></li>
+                        <li><a><img className="media-icon" src={linkedIn} alt="linkedIn logo" /></a></li>
+                    </ul>
+                </div>
             </div>
-        </footer>
+            <hr />
+            <div className="footer__policies">
+                <ul>
+                    <li><a>Privacy Policy</a></li>
+                    <li><a>Terms of Service</a></li>
+                    <li><a>Cookie Policy</a></li>
+                </ul>
+                <p> &copy; 2025 AES. All rights reserved</p>
+            </div>
+        </footer >
     );
 }
 
